83b644a5866657b918c56986b48f421639c0b9b5
Raki丶 <cats1821086364@gmail.com>
2025-05-27 17:34:48 +0200

    [ADD] ADD interaction sample packages and basic game UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Fusion;
+
+namespace Scripts
+{
+    public class GameManager : NetworkBehaviour
+    {
+    
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        void SpawnCube()
+        {
+
+        }
+
+        public void StartUI() {
+            Debug.Log("MQTT: ");
+        }
+    }
+}
+
